7c65fb143fabd53fd7604ed107a37ed0e5e9bc3b
Abadat Hossain <abadat26@gmail.com>
2024-10-12 12:41:11 +0600

    featured jobs are shown in home page

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import Job from "../Job/Job"
+
+export default function FeaturedJobs() {
+    const [jobs, setJobs]=useState([])
+
+    useEffect(()=>{
+        fetch('jobs.json')
+        .then(res=>res.json())
+        .then(data=>setJobs(data))
+    },[])
+  return (
+   <>
+    <div className="text-center">
+       <h1 className="text-4xl font-bold">FeaturedJobs:{jobs.length}</h1>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, odit.</p>
+       <div className="grid grid-cols-2">
+        {
+            jobs.map(job=><Job key={job.id} job={job}></Job>)
+        }
+       </div>
+    </div>
+   </>
+  )
+}
